23ab443055866903dcaa11ea51ffadc01e915e65
endodog <endodog.dev@gmail.com>
2024-03-18 02:29:59 +0700

    add employee pages

---
+++ src/pages/dashboard/employee/EmployeeList.js
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import { Link as RouterLink, useNavigate } from 'react-router-dom';
+
+// routes
+import { PATH_DASHBOARD } from '../../../routes/paths.js';
+
+export default function EmployeeList() {
+
+  return (
+    <div style={{ backgroundColor: "white" }}>
+      "Hello world"
+    </div>
+  )
+}
